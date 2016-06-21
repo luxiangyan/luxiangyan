9a56314bd9f4403e28f597d22e1fec8eb3eb9a81
dc002 <dc002@zhanshijiqi.lan>
2016-06-21 16:52:36 +0800

    Initial Commit

---
+++ L——heras/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L——heras
+//
+//  Created by dc002 on 16/6/21.
+//  Copyright © 2016年 Jocelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L——heras/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L——heras
+//
+//  Created by dc002 on 16/6/21.
+//  Copyright © 2016年 Jocelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
